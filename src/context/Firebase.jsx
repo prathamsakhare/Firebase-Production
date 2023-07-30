b6a0789cfabe43fb07d51b0035a0aad837ab2594
PrathamSakhare <psakhre@themohh.com>
2023-07-30 21:01:16 +0530

    added utility function for database using firebase realtime database /@prathamsakhare

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { initializeApp } from "firebase/app";
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
-
+import {getDatabase, set, ref} from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDeB0Eh-PiBtdylGO0JRQJ2dj7R0fAOT-g",
@@ -10,12 +10,13 @@ const firebaseConfig = {
   storageBucket: "fir-series-2.appspot.com",
   messagingSenderId: "83677404099",
   appId: "1:83677404099:web:1366f17676bfad4ebfbb76",
+  databaseURL : "https://fir-series-2-default-rtdb.firebaseio.com/",
 };
 
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
-
+const database = getDatabase(firebaseApp)
 
 
 const FirebaseContext = createContext(null)
@@ -25,8 +26,11 @@ export const FirebaseProvider = (props) => {
     const signUpUserWithEmailAndPassword = (email, password) => {
         return createUserWithEmailAndPassword(firebaseAuth, email, password)
     }
+
+    const putDataInDatabase = (key, data) => set(ref(database, key), data)
+
     return (
-        <FirebaseContext.Provider value={{signUpUserWithEmailAndPassword}}>
+        <FirebaseContext.Provider value={{signUpUserWithEmailAndPassword, putDataInDatabase}}>
             {props.children}
         </FirebaseContext.Provider>
     )
