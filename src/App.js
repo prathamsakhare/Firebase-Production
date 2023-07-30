a2ddf13bd0d335f40c5cdfe221ad06686647699b
PrathamSakhare <psakhre@themohh.com>
2023-07-30 22:08:38 +0530

    implemented and tested signup functionality using firebase authentication in App.js /@prathamsakhare

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useFirebase } from "./context/Firebase";
 
 function App() {
+  const firebase = useFirebase();
+
+
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
   return (
     <div className="App">
       <h1>Firebase</h1>
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        placeholder="Enter Email..."
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Enter Password..."
+      />
+      <button onClick={() => firebase.signUpUserWithEmailAndPassword(email, password)}>SignUp</button>
     </div>
   );
 }
--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,7 +1,7 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { initializeApp } from "firebase/app";
-import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
-import {getDatabase, set, ref} from 'firebase/database'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, set, ref } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDeB0Eh-PiBtdylGO0JRQJ2dj7R0fAOT-g",
@@ -10,28 +10,32 @@ const firebaseConfig = {
   storageBucket: "fir-series-2.appspot.com",
   messagingSenderId: "83677404099",
   appId: "1:83677404099:web:1366f17676bfad4ebfbb76",
-  databaseURL : "https://fir-series-2-default-rtdb.firebaseio.com/",
+  databaseURL: "https://fir-series-2-default-rtdb.firebaseio.com/",
 };
-
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
-const database = getDatabase(firebaseApp)
+const database = getDatabase(firebaseApp);
 
+const FirebaseContext = createContext(null);
 
-const FirebaseContext = createContext(null)
-
+// ! Custom hook for using firebase's utility functions
+export const useFirebase = () => useContext(FirebaseContext);
 
 export const FirebaseProvider = (props) => {
-    const signUpUserWithEmailAndPassword = (email, password) => {
-        return createUserWithEmailAndPassword(firebaseAuth, email, password)
-    }
+  const signUpUserWithEmailAndPassword = (email, password) => {
+    return createUserWithEmailAndPassword(firebaseAuth, email, password)
+      .then((res) => alert("User Created!"))
+      .catch((err) => alert(err));
+  };
 
-    const putDataInDatabase = (key, data) => set(ref(database, key), data)
+  const putDataInDatabase = (key, data) => set(ref(database, key), data);
 
-    return (
-        <FirebaseContext.Provider value={{signUpUserWithEmailAndPassword, putDataInDatabase}}>
-            {props.children}
-        </FirebaseContext.Provider>
-    )
-}
+  return (
+    <FirebaseContext.Provider
+      value={{ signUpUserWithEmailAndPassword, putDataInDatabase }}
+    >
+      {props.children}
+    </FirebaseContext.Provider>
+  );
+};
